f59a9eeac2bbb3d1596f790c027df43b89a6fad1
Danilo Pangardi <danilokz.tec@gmail.com>
2022-01-11 11:22:31 -0300

    adding jasypt to encrypt access key

--- src/main/java/br/com/jaya/currencyconverter/CurrencyConverterApplication.java
+++ src/main/java/br/com/jaya/currencyconverter/CurrencyConverterApplication.java
@@ -3,6 +3,9 @@ package br.com.jaya.currencyconverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class CurrencyConverterApplication {
 
